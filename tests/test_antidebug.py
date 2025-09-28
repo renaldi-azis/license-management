e75c40aa7ffd35e3c530258a15b16685e7e1596c
renaldi-azis <arifrenaldi0720@gmail.com>
2025-09-28 10:36:58 +0700

    'fix: fix exchange key'

--- tests/test_antidebug.py
+++ tests/test_antidebug.py
@@ -1,20 +1,26 @@
 import HttpAntiDebug
+from HttpAntiDebug import SessionServer as SV
 
-req = HttpAntiDebug.get("https://richtoolsmmo.com")
-print(req.status_code)
-print(req.text)
-print(req.headers)
+sess = SV('https://jsonip.com')
+
+print(sess.get('/').text)
+sess.get('https://jsonip.com',)
+
+# req = HttpAntiDebug.get("https://richtoolsmmo.com")
+# print(req.status_code)
+# print(req.text)
+# print(req.headers)
 
 import requests
 response = requests.get("https://103.152.165.248", verify=False)
 print(response.status_code)
 
-import socket
-try:
-    domain = socket.gethostbyaddr("103.152.165.248")
-    print(f"IP resolves to: {domain}")
-except:
-    print("Reverse DNS lookup failed")
+# import socket
+# try:
+#     domain = socket.gethostbyaddr("103.152.165.248")
+#     print(f"IP resolves to: {domain}")
+# except:
+#     print("Reverse DNS lookup failed")
 
 req = HttpAntiDebug.get("https://103.152.165.248")
 print(req.status_code)
